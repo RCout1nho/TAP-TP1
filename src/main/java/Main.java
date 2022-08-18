77d936d94ff6e5ba2424bd75afe7098d09a7a54d
Jose Coutinho <jcout@motorola.com>
2022-08-18 09:40:34 -0400

    refactor: add @author

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,10 @@
-import model.User;
-import model.enumerators.UserTypeEnum;
 import view.*;
 
+/**
+ * @author ricardo.coutinho@icomp.ufam.edu.br
+ */
 public class Main {
     public static void main(String[] args) {
-//        User user = new User(1, "ricardo", "ricardo@example.com", UserTypeEnum.ADMIN, "123");
         LoginView login = new LoginView();
-//        Title title = new Title(user);
-//        NewLoginView newLoginView = new NewLoginView();
-//        Rent rent = new Rent(user);
-//        HomeView home = new HomeView(user);
-//        UserView viewUserView = new UserView(user);
     }
 }
